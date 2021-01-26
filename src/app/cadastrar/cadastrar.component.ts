dbf24d0c3dd9879c8ab8d9346be82832ab3f9421
Andressa Amorim <dev.andymal@gmail.com>
2021-01-26 11:52:25 -0300

    Componente início sem consumo

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -3,13 +3,14 @@ import { Router } from '@angular/router';
 import { User } from '../model/User';
 import { AuthService } from '../service/auth.service';
 
+
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css'],
 })
 export class CadastrarComponent implements OnInit {
-  
+
   user: User = new User();
   confirmarSenha: string
   tipoUsuario: string
@@ -17,7 +18,7 @@ export class CadastrarComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private rotas: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
     window.scroll(0, 0);
@@ -27,22 +28,33 @@ export class CadastrarComponent implements OnInit {
     this.confirmarSenha = event.target.value
   }
 
-  tipoUser(event: any){
+  tipoUser(event: any) {
     this.tipoUsuario = event.target.value
   }
 
-  cadastrar(){
+  cadastrar() {
     this.user.tipo = this.tipoUsuario
 
-    if(this.user.senha != this.confirmarSenha){
+    if (this.user.senha != this.confirmarSenha) {
       alert('Senhas incorretas')
     } else {
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
         this.rotas.navigate(['/entrar'])
         alert('Usuário cadastrado com sucesso!')
+      }, erro => {
+        if(erro.status == 500){
+          alert('Cadastro incompleto ou usuário já cadastrado')
+        }
       })
     }
+  }
 
+  verificarUser() { 
+    this.user.tipo = this.tipoUsuario
+
+    if (this.user.usuario.length <= 4) {
+      alert('Nome de usuário menor que 5 caracteres')
+    }
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
 
 export const environment = {
   production: true,
